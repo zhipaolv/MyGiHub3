eced2aa07ee201adbc6bdea81ceb826259854501
zhipaolv <lvzhipao@gmail.com>
2017-01-03 21:55:06 -0800

    3rd commit

--- src/test/java/zhipao/HelloWorld.java
+++ src/test/java/zhipao/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld{
 		} else if (args[0].equals("Reverse")){
 
 			System.out.println(reverse2(args[1]));
-			
+
 		} else {
 			System.out.println("您输入的指令有误");
 			System.out.println("Greetings指令--输出欢迎信息");
@@ -26,18 +26,18 @@ public class HelloWorld{
 		String reverse = "";
 		for(int i = 0; i < length; i++)
 			reverse = s.charAt(i) + reverse;
-		return reverse; 
+		return reverse;
 	}
 
 	public static String reverse2(String s){
-		StringBuffer str = new StringBuffer(s); 
+		StringBuffer str = new StringBuffer(s);
 		int length = s.length();
 		for(int i = 0; i < Math.floor(length/2); i++){
 			char j = s.charAt(i);
 			str.setCharAt(i,s.charAt(length-i-1));
 			str.setCharAt(length-i-1, j);
 		}
-		return str.toString(); 
+		return str.toString();
 	}
 
 
